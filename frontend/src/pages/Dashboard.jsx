75dd327bb8b1f95ddf053cafdb901729ca204e13
Udo Chigbu <udochukwuchigbu@yahoo.com>
2026-03-03 18:10:18 -0500

    Hashed passwords

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/Dashboard.css";
+
+function Dashboard(){
+
+
+
+
+
+
+
+
+return(
+    <div className="background">
+        <h1>Application Hub</h1>
+        <p>Welcome,</p>
+
+
+
+
+
+    </div>
+)
+
+
+
+
+}
+export default Dashboard;
